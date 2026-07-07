f6155fc2763969c19fcd42cd4016760e792ce4e8
Nik Erlandsson <44620116+nikerl@users.noreply.github.com>
2026-07-07 22:19:52 +0200

    Implement Branch engine

--- src/exec/branch.rs
+++ src/exec/branch.rs
@@ -1,10 +1,57 @@
-use crate::{data::{memory::Memory, rf_scalar::ScalarRF}, instruction_set::Instruction, exec::ScalarFU};
+use crate::{
+    data::{
+        memory::Memory, 
+        rf_scalar::ScalarRF
+    }, 
+    instruction_set::{
+        Instruction, 
+        InstructionSet::*
+    }, 
+    exec::ScalarFU
+};
+
 
 pub struct Branch;
 
 // BEQ, BNE, BLT, BGE, BLTU, BGEU
 impl ScalarFU for Branch {
     fn execute(instr: Instruction, regs: &mut ScalarRF, mem: &mut Memory) {
-        todo!()
+        let offset = instr.im1;
+        let rs1 = instr.rs1 as usize;
+        let rs2 = instr.rs2 as usize;
+
+        match instr.opcode {
+            BEQ => {
+                if regs[rs1] == regs[rs2] {
+                    regs.pc = (regs.pc as i32).wrapping_add(offset) as u32;
+                }
+            }
+            BNE => {
+                if regs[rs1] != regs[rs2] {
+                    regs.pc = (regs.pc as i32).wrapping_add(offset) as u32;
+                }
+            }
+            BLT => {
+                if (regs[rs1] as i32) < (regs[rs2] as i32) {
+                    regs.pc = (regs.pc as i32).wrapping_add(offset) as u32;
+                }
+            }
+            BGE => {
+                if (regs[rs1] as i32) >= (regs[rs2] as i32) {
+                    regs.pc = (regs.pc as i32).wrapping_add(offset) as u32;
+                }
+            }
+            BLTU => {
+                if regs[rs1] < regs[rs2] {
+                    regs.pc = regs.pc.wrapping_add(offset as u32);
+                }
+            }
+            BGEU => {
+                if regs[rs1] >= regs[rs2] {
+                    regs.pc = regs.pc.wrapping_add(offset as u32);
+                }
+            }
+            _ => println!("Unrecognized opcode")
+        }
     }
 }
