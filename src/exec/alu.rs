292a4faa20e21fdb10cb9ddf9163b9926283ef4c
Nik Erlandsson <44620116+nikerl@users.noreply.github.com>
2026-07-07 16:21:54 +0200

    Implement ALU

--- src/exec/alu.rs
+++ src/exec/alu.rs
@@ -1,11 +1,90 @@
-use crate::{data::{memory::Memory, rf_scalar::ScalarRF}, instruction_set::Instruction, exec::ScalarFU};
+use crate::{
+    data::{
+        memory::Memory, 
+        rf_scalar::ScalarRF
+    }, 
+    instruction_set::{
+        Instruction, 
+        InstructionSet::*
+    }, 
+    exec::ScalarFU
+};
 
 pub struct Alu;
 
-// ADD, SUB, XOR, OR, AND, SLL, SRL, SRA, SLT, SLTU, MUL, MULH, MULHSU, MULHU, DIV, DIVU, REM, REMU, ADDI, XORI, ORI, ANDI, SLLI, SRLI, SRAI, SLTI, SLTIU, LUI, AUIPC
 impl ScalarFU for Alu {
-    fn execute(instr: Instruction, regs: &mut ScalarRF, mem: &mut Memory) {
+    fn execute(instr: Instruction, regs: &mut ScalarRF, _mem: &mut Memory) {
         println!("instruction dispatched to alu: {:#?}", instr);
-        //todo!()
+
+        let im1 = instr.im1;
+        let rs1 = instr.rs1 as usize;
+        let rs2 = instr.rs2 as usize;
+        let rd = instr.rd as usize;
+        
+        match instr.opcode {
+            ADD => regs[rd] = regs[rs1].wrapping_add(regs[rs2]),
+            SUB => regs[rd] = regs[rs1].wrapping_sub(regs[rs2]),
+            XOR => regs[rd] = regs[rs1] ^ regs[rs2],
+            OR => regs[rd] = regs[rs1] | regs[rs2],
+            AND => regs[rd] = regs[rs1] & regs[rs2],
+            SLL => regs[rd] = regs[rs1] << (regs[rs2] & 0b11111),
+            SRL => regs[rd] = regs[rs1] >> (regs[rs2] & 0b11111),
+            SRA => regs[rd] = ((regs[rs1] as i32) >> (regs[rs2] & 0b11111)) as u32,
+            SLT => if (regs[rs1] as i32) < (regs[rs2] as i32) {regs[rd] = 1} else {regs[rd] = 0}
+            SLTU => if regs[rs1] < regs[rs2] {regs[rd] = 1} else {regs[rd] = 0}
+            MUL => regs[rd] = regs[rs1].wrapping_mul(regs[rs2]),
+            MULH => {
+                let result = (regs[rs1] as i32 as i64) * (regs[rs2] as i32 as i64);
+                regs[rd] = (result >> 32) as u32;
+            }
+            MULHSU => {
+                let result = (regs[rs1] as i32 as i64) * (regs[rs2] as i64); // rs2 zero filled
+                regs[rd] = (result >> 32) as u32;
+            }
+            MULHU => {
+                let result = (regs[rs1] as u64) * (regs[rs2] as u64);
+                regs[rd] = (result >> 32) as u32;
+            }
+            DIV => {
+                if regs[rs2] == 0 {
+                    regs[rd] = u32::MAX; // division by zero
+                } else {
+                    regs[rd] = (regs[rs1] as i32).wrapping_div(regs[rs2] as i32) as u32;
+                }
+            }
+            DIVU => {
+                if regs[rs2] == 0 {
+                    regs[rd] = u32::MAX; // division by zero
+                } else {
+                    regs[rd] = regs[rs1].wrapping_div(regs[rs2]);
+                }
+            }
+            REM => {
+                if regs[rs2] == 0 {
+                    regs[rd] = regs[rs1]; // division by zero
+                } else {
+                    regs[rd] = ((regs[rs1] as i32).wrapping_rem(regs[rs2] as i32)) as u32;
+                }
+            }
+            REMU => {
+                if regs[rs2] == 0 {
+                    regs[rd] = regs[rs1]; // division by zero
+                } else {
+                    regs[rd] = regs[rs1].wrapping_rem(regs[rs2]);
+                }
+            }
+            ADDI => regs[rd] = regs[rs1].wrapping_add(im1 as u32),
+            XORI => regs[rd] = regs[rs1] ^ im1 as u32,
+            ORI => regs[rd] = regs[rs1] | im1 as u32,
+            ANDI => regs[rd] = regs[rs1] & im1 as u32,
+            SLLI => regs[rd] = regs[rs1] << im1 as u32,
+            SRLI => regs[rd] = regs[rs1] >> im1 as u32,
+            SRAI => regs[rd] = ((regs[rs1] as i32) >> im1) as u32,
+            SLTI => if (regs[rs1] as i32) < im1 {regs[rd] = 1} else {regs[rd] = 0},
+            SLTIU => if regs[rs1] < (im1 as u32) {regs[rd] = 1} else {regs[rd] = 0},
+            LUI => regs[rd] = im1 as u32,
+            AUIPC => regs[rd] = regs.pc.wrapping_add(im1 as u32),
+            _ => println!("Unrecognized opcode")
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,16 @@ fn main() {
     let mut rf = ScalarRF::new();
     let mut mrf = MatrixRF::new();
 
+
+    rf[12] = 9; rf[13] = 10; 
     // example: ADD a11, a12, a13
     let instruction: Instruction = decoder::decode(0b0000_0000_1101_0110_0000_0101_1011_0011);
 
+    println!("{:?}", instruction);
+
     dispatcher::dispatch(instruction, &mut rf, &mut mrf, &mut mem);
+
+    println!("add instr result: {} + {} = {}", rf[12], rf[13], rf[11]);
 
     rf[1] = 42;
     rf.set_sp(0x1234_ABCD); 
